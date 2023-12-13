e6db7065c29395176549b006474ec1669ceca9e1
Cesar Augusto Palacios Alarcon <ceapalaciosal@gmail.com>
2023-12-12 20:09:29 -0500

    include api and dashboard

--- js/axios2.js
+++ js/axios2.js
@@ -7,13 +7,16 @@ function Personajes(character){
     const container = document.getElementById('personajes')
     character.map(personajillo => {
         container.innerHTML +=`
+            <div class="col">
             <div class="card" style="width: 18rem;">
                 <img class="card-img-top" src=${personajillo.image} alt="Card image cap">
                 <div class="card-body">
                     <p class="card-text">${personajillo.name}</p>
                 </div>
-        </div>`});
-        
+        </div>
+        </div>
+        `});
+
     // container.innerHTML = `
     // <div class="row">
     //     <div class="col-sm">
---
+++ js/ejemplo.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function(){
+    const nombreInput = document.getElementById("Email");
+    
+});
