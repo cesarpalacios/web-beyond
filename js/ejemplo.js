8341a829bd699d30898619d971b63d3149dae2e7
Cesar Augusto Palacios Alarcon <ceapalaciosal@gmail.com>
2023-12-12 23:42:53 -0500

    update login

--- js/ejemplo.js
+++ js/ejemplo.js
@@ -1,11 +1,15 @@
 
 function go(){
-    
-    
-    alert(document.form.user.value, document.form.password.value)
-    console.log(document.form.password.value)
 
-
-
+    if (document.form.user.value  && document.form.password.value ){
+        //alert(document.form.user.value, document.form.password.value);
+        alert("bienvenido " + document.form.user.value);
+        //window.location="rickandmorty.html";
+        location.href="startbootstrap-sb-admin-2-gh-pages/index.html";
+        //alert(location.href)
+        //location.replace('rickandmorty.html')
+    }else{
+        alert("ingrese datos en usuario y contraseña");
+    };
 
 } 
