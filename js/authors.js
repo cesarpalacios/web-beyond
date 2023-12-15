0fc3407402d37ea19a2bb7971e97f33c82608acf
Cesar Augusto Palacios Alarcon <ceapalaciosal@gmail.com>
2023-12-14 19:52:32 -0500

    se sube get and post

---
+++ js/authors.js
@@ -0,0 +1,52 @@
+//document.getElementById('authors')?.innerHTML('hola')
+
+fetch('https://8ffa-138-84-41-69.ngrok.io/get_books')
+    .then(response => response.json())
+    .then(json => authors_get(json.books))
+
+
+function authors_get(lista){
+    const container = document.getElementById('autores')
+    lista.map( author => {
+        container.innerHTML += `
+          <tr>
+            <td>${author.id}</td>
+            <th>${author.author_id}</th>
+            <td>${author.title}</td>
+          </tr>
+        `
+       
+    }
+    )
+
+}
+
+
+function authors_post(){
+   
+    if (document.form.id_autor.value && document.form.nombre_libro_autor.value){
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+        "title": document.form.nombre_libro_autor.value,
+        "author_id": parseInt(document.form.id_autor.value)
+        });
+    
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+        };
+    
+        fetch("https://8ffa-138-84-41-69.ngrok.io/insert_books", requestOptions)
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(error => alert('error', error));
+    }else{
+        alert('Ingerese el nombre del autor')
+    }
+    
+
+}
+
--- js/ejemplo.js
+++ js/ejemplo.js
@@ -1,7 +1,7 @@
 
 function go(){
 
-    if (document.form.user.value  && document.form.password.value ){
+    if (document.form.user.value == 'ceapalaciosal@gmail.com' && document.form.password.value == '12345' ){
         //alert(document.form.user.value, document.form.password.value);
         alert("bienvenido " + document.form.user.value);
         //window.location="rickandmorty.html";
